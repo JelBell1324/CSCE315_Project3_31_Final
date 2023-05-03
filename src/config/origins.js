d96a2e574070c616a334e37512b78638e3673576
cherry13245 <cherrytree1324@tamu.edu>
2023-05-03 15:10:30 -0500

    orgins

--- src/config/origins.js
+++ src/config/origins.js
@@ -2,6 +2,6 @@ import DEBUG from "./debug";
 /**
  * Check backendOrgin
  */
-const backendOrigin = DEBUG ? 'http://localhost:8080' : 'https://csce315-project3-31-backend-production.up.railway.app';
-console.log(backendOrigin);
+const backendOrigin = DEBUG ? 'http://localhost:8080' : 'https://csce315-project3-backend-31-production.up.railway.app/';
+if (DEBUG) console.log(backendOrigin);
 export default backendOrigin;
